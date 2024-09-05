f0edc67b5a7a81b00b90ea7f3bfef17771be606a
Yehezkel Ben Atar <hezi3188@gmail.com>
2024-09-05 17:17:30 +0300

    feat(): add skelton

---
+++ src/pages/addPlace/addPlace.tsx
@@ -0,0 +1,5 @@
+const AddPlace = () => {
+	return <div className='flex  bg-slate-600 w-50 mx-auto px-4'>add place </div>;
+};
+
+export default AddPlace;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+const flowbite = require('flowbite-react/tailwind');
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', flowbite.content()],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		flowbite.plugin(),
+		function ({ addUtilities }) {
+			addUtilities({
+				'.rtl': {
+					direction: 'rtl',
+				},
+				'.ltr': {
+					direction: 'ltr',
+				},
+			});
+		},
+	],
 };
